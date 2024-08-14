707643e3aa743f27b7d09372ab25f7102e8fbf6b
sumch76 <sumitchaubey76@gmail.com>
2024-08-14 12:00:06 +0530

    day-5 commit

---
+++ Day-5/day-5.js
@@ -0,0 +1,81 @@
+// Tasks | Activities 🌟
+// Activity 1: Function Declaration
+
+// Task 1: Write a function to check if a number is even or odd and log the result to the console.
+ 
+    function checkEvenOdd(num)
+    {
+        if(num%2===0)
+            console.log(`${num} is even`);
+            else
+            console.log(`${num} is odd`);
+            
+    }
+    checkEvenOdd(15);
+    checkEvenOdd(20);
+
+// Task 2: Write a function to calculate the square of a number and return the result.
+ function square(num)
+ {
+    console.log(`square of ${num} is :`+num*num);
+    
+ }
+
+ square(20);
+ square(25);
+// Task 3: Write a function to find the maximum of two numbers and return the result.
+
+function  maximum(num1,num2)
+{
+    if(num1>num2)
+        console.log(`maximum is ${num1}`);
+    else
+    console.log(`maximum is ${num2}`);
+        
+}
+maximum(10,45);
+maximum(567,78);
+
+
+
+// Task 4: Write a function expression to concatenate two strings and return the result.
+const concatenateStr=function(str1,str2)
+{
+    return str1 + str2;
+}
+
+console.log(concatenateStr("hello","duiyaaaaa"));
+
+
+
+// Activity 3: Arrow Functions
+
+// Task 5: Write an arrow function to calculate the sum of two numbers and return the result.
+
+const sum=(num11,num22)=>{
+    return num11+num22;
+}
+
+console.log(sum(10,20));
+
+
+// Task 6: Write an arrow function to check if a string contains a specific character and return a boolean value.
+
+
+// Activity 4: Function Parameters and Default Values
+
+// Task 7: Write a function that takes two parameters and returns their product. Provide a default value for the second parameter.
+
+
+// Task 8: Write a function that takes a person's name and age and returns a greeting message. Provide a default value for the age.
+
+
+
+// Activity 5: Higher-Order Functions
+
+// Task 9: Write a higher-order function that takes a function and a number, and calls the function that many times.
+
+
+
+
+// Task 10: Write a higher-order function that takes two functions and a value, applies the first function to the value, and then applies the second function to the result.
