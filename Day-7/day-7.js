d38020f17637a9c455c8f24d3ff44685da456074
sumch76 <sumitchaubey76@gmail.com>
2024-08-17 23:03:51 +0530

    day-7 commit

--- Day-7/day-7.js
+++ Day-7/day-7.js
@@ -81,12 +81,26 @@ library.books.forEach( function (book){
 
 // Task 7: Add a method to the book object that uses the this keyword to return a string with the book's title and year, and log the result of calling this method.
 
+book.getBookInfo=function()
+{
+    return `Title : ${this.title}, Author : ${this.author}`;
+}
+console.log(book.getBookInfo());
+
+
+
+
+
 
 // Activity 5: Object Iteration
 
 // Task 8: Use a for...in loop to iterate over the properties of the book object and log each property and its value.
 
-
+for(let key in book)
+    console.log(key + " : " + book[key]);
 
 // Task 9: Use Object.keys and Object.values methods to log all the keys and values of the book object.
 
+console.log("keys : " + Object.keys(book));
+
+console.log("values : " + Object.values(book));
