63ebac20204626e8a93745909a3fd2936d864954
sumch76 <sumitchaubey76@gmail.com>
2024-08-19 09:21:47 +0530

    dom task

---
+++ DOM Task/task1/index.js
@@ -0,0 +1,24 @@
+// let btn=document.querySelector("button");
+// btn.addEventListener("click",function(){
+//     let para=document.querySelector("p");
+//     para.textContent="task 1 is done";
+//     para.style.color="red";
+
+//     // setTimeout(()=>{
+//     //     btn.disabled=false;
+//     //     btn.style.backgroundColor="green";
+//     //     para.textContent="click me";
+//     // },5000);
+// }
+//     );
+
+
+//using arrow function
+    var btn=document.querySelector("button");
+    var para=document.querySelector("p");
+    btn.addEventListener("click",()=>
+    {
+        para.textContent="task 1 is done"
+        para.style.color="red";
+    });
+
