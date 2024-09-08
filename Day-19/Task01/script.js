4d791d9a5b3129644da20c27ff272f01cd0fcc01
sumch76 <sumitchaubey76@gmail.com>
2024-09-08 12:06:41 +0530

    day-19

---
+++ Day-19/Task01/script.js
@@ -0,0 +1,12 @@
+// Task 1: 
+// Write a script to save a string value to localStorage and retrieve it. Log the retrieved value.
+// localStorage is a part of web Storage api that allows in js to store key-value pairs in a web browser
+
+const valueToSave="hello ,localStorage";
+localStorage.setItem("myString", valueToSave);
+
+//here setItem accepting two strings key(myString)
+// and VALUE(valuetoSave)
+const retrievedItem=localStorage.getItem("myString");
+
+console.log(retrievedItem);
