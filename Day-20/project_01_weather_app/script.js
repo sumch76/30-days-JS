a3123501229dc7281c8e83fc6143d75fba8f4cb6
sumch76 <sumitchaubey76@gmail.com>
2024-09-13 22:44:40 +0530

    weather-app

--- Day-20/project_01_weather_app/script.js
+++ Day-20/project_01_weather_app/script.js
@@ -12,9 +12,20 @@ function fetchWeatherData(city){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`)
     .then((response)=>response.json())
     .then((data)=>{
-        // document.getElementById("city-name").textContent = data.name;
-        // document.getElementById("temp").textContent=`Temperature :`
-        console.log(data);
+        document.getElementById("city-name").textContent = data.name;
+        document.getElementById("temp").textContent=`Temperature : ${data.main.temp}  C`;
+        const weatherCondition=data.weather[0].description;
+        document.getElementById("weather-condition").textContent=weatherCondition;
+ fetchForecastData(city);
         
-    })
+    }).catch((err)=>console.error("error fetcging data",err));
+    
+}
+
+function fetchForecastData(city){
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}&units=metric`)
+   .then((response)=>response.json())
+   .then((data)=>{
+    
+   })
 }
