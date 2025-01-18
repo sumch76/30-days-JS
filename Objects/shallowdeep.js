a9682257570a1d7e90b153ee165cac82c914209b
sumch76 <sumitchaubey76@gmail.com>
2025-01-18 17:25:48 +0530

    deep copy ensures alll levels of the object including nested obj are cloned into aa completely new object

--- Objects/shallowdeep.js
+++ Objects/shallowdeep.js
@@ -37,3 +37,32 @@ obj2.name="hardik";
 
 console.log(obj2);
 console.log(obj);
+//nested structure
+//if a property contains a nested object or array, the nested structure is not copied but shared
+
+var obj = {
+    name: "sumit",
+    age: 25,
+    address: { city: "Delhi" }
+};
+var obj2 = { ...obj };
+obj2.address.city = "Mumbai";
+
+console.log(obj);  // { name: "sumit", age: 25, address: { city: "Mumbai" } }
+console.log(obj2); // { name: "sumit", age: 25, address: { city: "Mumbai" } }
+
+//it can solve using deep copy which ensures that all levels of the object including nested structure are cloned into a completely nested object
+
+var obj={
+    name:"sumit",
+    age:25,
+    address:{
+        city:"delhi"
+    }
+};
+var obj=JSON.parse(JSON.stringify(obj));
+obj2.name="hardik";
+obj2.address.city="uttarakhand";
+console.log(obj);
+console.log(obj2);
+
