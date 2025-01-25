03a157b18989c931871f203714eb883f0d27672c
sumch76 <sumitchaubey76@gmail.com>
2025-01-25 15:47:28 +0530

    prototype of map🗺️

---
+++ Pollyfills/map.js
@@ -0,0 +1,16 @@
+//pollyfill for map()
+//Array.map((num,i,arr)=>{ })
+
+Array.prototype.myMap=function (cb) 
+{
+    let temp=[];
+    for (let i = 0; i < this.length; i++) {
+        temp.push(cb(this[i]));
+        
+    }
+    return temp;
+    
+};
+const nums=[1,2,4,5];
+const multiply=nums.myMap((nums)=>nums*3)
+console.log(multiply);
